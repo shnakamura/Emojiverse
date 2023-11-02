8d20a6083f83babd75653200a1d85f240bf52444
shnakamura <joaovtnakamura@gmail.com>
2023-11-02 17:40:24 -0300

    Stop using tag handlers in favor of custom tags

--- Emojiverse.cs
+++ Emojiverse.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
@@ -12,6 +14,8 @@ public sealed class Emojiverse : Mod
     public static string EmojiPath => Path.Combine(Main.SavePath, "Emojis");
 
     public override void Load() {
+        On_ChatManager.ParseMessage += ParseMessageHook;
+        
         if (Directory.Exists(EmojiPath)) {
             return;
         }
@@ -19,7 +23,28 @@ public sealed class Emojiverse : Mod
         Directory.CreateDirectory(EmojiPath);
     }
 
-    public override void PostSetupContent() {
-        ChatManager.Register<EmojiTagHandler>("e", "emoji");
+    private List<TextSnippet> ParseMessageHook(On_ChatManager.orig_ParseMessage orig, string text, Color baseColor) {
+        const string pattern = "^:(.*):$";
+        
+        var snippets = orig(text, baseColor);
+        var newSnippets = new List<TextSnippet>(snippets);
+
+        foreach (var snippet in snippets) {
+            var snippetText = snippet.Text;
+            var match = Regex.Match(snippetText, pattern);
+
+            if (!match.Success) {
+                continue;
+            }
+            
+            var name = match.Groups[1].Value;
+                
+            newSnippets.Add(new EmojiSnippet(name) {
+                CheckForHover = true,
+                DeleteWhole = true
+            });
+        }
+        
+        return newSnippets;
     }
 }
