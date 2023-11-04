f2b97e97a5e16723cf0f288781feb6c01908b257
shnakamura <joaovtnakamura@gmail.com>
2023-11-04 10:11:43 -0300

    Main menu bugfix + Rendering progress

--- Common/Chat/EmojiSnippet.cs
+++ Common/Chat/EmojiSnippet.cs
@@ -1,55 +1,49 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Graphics;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.UI;
 using Terraria.UI.Chat;
 
 namespace Emojiverse.Common.Chat;
 
 public sealed class EmojiSnippet : TextSnippet
 {
-    // private const float Size = 30f;
-    // private const float SizeLimit = Size * 0.75f;
-    
-    private const float Padding = 4f;
+    private const float Size = 30f;
+    private const float SizeLimit = Size * 0.75f;
 
     public override void OnHover() {
-        Main.instance.MouseText("");
+        Main.instance.MouseText("missing");
     }
 
     public override bool UniqueDraw(bool justCheckingString, [UnscopedRef] out Vector2 size, SpriteBatch spriteBatch, Vector2 position = default, Color color = default, float scale = 1f) {
-        var Size = 32f;
-        var SizeLimit = 24f;
-        
-        if (!justCheckingString) {
+        var validColor = color.R != 0 || color.G != 0 || color.B != 0;
+
+        if (!justCheckingString && validColor) {
             var texture = ModContent.Request<Texture2D>($"{nameof(Emojiverse)}/Assets/Textures/Missing").Value;
-        
+
             var frame = texture.Frame();
             var origin = frame.Size() / 2f;
-        
+
             var modifiedDrawScale = 1f;
-        
-            if ((float)frame.Width > SizeLimit || (float)frame.Height > SizeLimit) {
-                modifiedDrawScale = ((frame.Width <= frame.Height) ? (SizeLimit / (float)frame.Height) : (SizeLimit / (float)frame.Width));
+
+            if (frame.Width > SizeLimit || frame.Height > SizeLimit) {
+                modifiedDrawScale = frame.Width <= frame.Height ? SizeLimit / frame.Height : SizeLimit / frame.Width;
             }
-        
+
             var finalDrawScale = scale * modifiedDrawScale;
-            var offset = (texture.Size() * finalDrawScale) / 2f;
-            
+            var offset = texture.Size() * finalDrawScale / 2f;
+
             spriteBatch.Draw(texture, position + offset, frame, Color.White, 0f, origin, finalDrawScale, SpriteEffects.None, 0f);
         }
-        
+
         size = new Vector2(Size) * scale * 0.75f;
-        
+
         return true;
     }
 
     public override float GetStringLength(DynamicSpriteFont font) {
-        return 30 * Scale;
+        return Size * Scale * 0.75f;
     }
 }
