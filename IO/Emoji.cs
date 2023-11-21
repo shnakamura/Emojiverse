dbd36c3cf4b5530071c6f41a5f3d100828ce152b
shnakamura <joaovtnakamura@gmail.com>
2023-11-20 22:14:07 -0300

    sealed class => readonly record struct

--- IO/Emoji.cs
+++ IO/Emoji.cs
@@ -2,23 +2,4 @@
 
 namespace Emojiverse.IO;
 
-public sealed class Emoji
-{
-    public string Name { get; }
-    public string Alias { get; }
-    public string Path { get; }
-    public string Pack { get; }
-    
-    public int Id { get; }
-    
-    public bool Animated { get; }
-
-    internal Emoji(string name, string alias, string path, string pack, int id, bool animated) {
-        Name = name;
-        Alias = alias;
-        Path = path;
-        Pack = pack;
-        Id = id;
-        Animated = animated;
-    }
-}   
+public readonly record struct Emoji(string Name, string Alias, string Path, string Pack, int Id, bool Animated);
